36a887502cc20d263864ccb8c3d3dd660fc45594
Brito79 <bshayamano2002@gmail.com>
2024-03-23 03:37:33 -0700

    2-remove_database.sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- deletes a database 
--- if the databsae does not exist, it should not fail
-DELETE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- if the databsae does not exist, it should not faiIL
+DROP DATABASE IF EXISTS hbtn_0c_0;
