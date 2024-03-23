4e110e6d56181e7997c87fe760514ec7bbc259bd
Brito79 <bshayamano2002@gmail.com>
2024-03-23 05:18:01 -0700

    6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -2,4 +2,4 @@
 -- from the database hbtn_0c_0 in your MySQL server.
 -- All fields should be printed
 -- The database name will be passed as an argument of the mysql command
-SELECT * FROM hbtn_0c_0;
+SELECT * FROM first_table;
