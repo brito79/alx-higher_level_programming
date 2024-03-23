9889e2d7e111b097f2651a0103a43cca053e6a4b
Brito79 <bshayamano2002@gmail.com>
2024-03-23 04:56:13 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- a script that prints the full description of the table
--- first_table in my MySQL Server server
-SHOW COLUMNS  FROM hbtn_0c_0;
+-- first_table in my MySQL Server serveR
+SHOW COLUMNS FROM hbtn_0c_0.first_table;
